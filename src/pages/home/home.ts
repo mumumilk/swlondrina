42a376fc88390861dc9c7122501470364423f887
Mar Lubanco <lubanco@edu.unifil.br>
2018-03-17 19:12:22 -0300

    Atualizações e navegação para cardapio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CardapioPage } from '../cardapio/cardapio';
+
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,11 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+  goCardapio(){
+    this.navCtrl.push(CardapioPage);
+      }
+  
+
 
 }
