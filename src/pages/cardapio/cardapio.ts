abb3ac73dd9fddb70d9290431eecf71702da459c
samuel koch <samuel.souza.koch@gmail.com>
2018-03-17 19:13:04 -0300

    colocando preco no cardapio

--- src/pages/cardapio/cardapio.ts
+++ src/pages/cardapio/cardapio.ts
@@ -8,16 +8,16 @@ export class CardapioPage implements OnInit {
     tipos = [
         {
             nome: 'Massas', itens: [
-                { nome: 'macarrao', sub: '2 molhos', img: 'assets/imgs/macarrao.jpg' },
-                { nome: 'Nhoque', sub: 'Frito, 2 molhos', img: 'assets/imgs/nhoque.jpg' },
-                { nome: 'Pastelzinho', sub: 'Frito', img: 'assets/imgs/pastelzinho.jpg' },
-                { nome: 'Pizza', sub: '2 sabores', img: 'assets/imgs/pizza.jpg' }
+                { nome: 'macarrao', sub: '2 molhos', img: 'assets/imgs/macarrao.jpg', preco: '10.90' },
+                { nome: 'Nhoque', sub: 'Frito, 2 molhos', img: 'assets/imgs/nhoque.jpg', preco: '05.90' },
+                { nome: 'Pastelzinho', sub: 'Frito', img: 'assets/imgs/pastelzinho.jpg', preco: '10.90' },
+                { nome: 'Pizza', sub: '2 sabores', img: 'assets/imgs/pizza.jpg', preco: '20.90' }
             ]
         },
         {
             nome: 'Bebidas', itens: [
-                { nome: 'Coca cola', sub: 'Lata', img: 'assets/imgs/coca.jpg' },
-                { nome: 'Cerveja', sub: '600 ml', img: 'assets/imgs/cerveja.jpg' }
+                { nome: 'Coca cola', sub: 'Lata', img: 'assets/imgs/coca.jpg', preco: '5.00' },
+                { nome: 'Cerveja', sub: '600 ml', img: 'assets/imgs/cerveja.jpg', preco: '6.90' }
             ]
         }
     ]
@@ -25,7 +25,7 @@ export class CardapioPage implements OnInit {
     constructor() {
 
     }
-
+ 
     ngOnInit() {
 
     }
