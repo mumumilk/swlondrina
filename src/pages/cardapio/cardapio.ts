a9cfbb5d27c54bdaa636badf6d29a9fc94554031
samuel koch <samuel.souza.koch@gmail.com>
2018-03-18 12:56:12 -0300

    colocando a descricao nos itens

--- src/pages/cardapio/cardapio.ts
+++ src/pages/cardapio/cardapio.ts
@@ -9,27 +9,38 @@ import { NavController } from "ionic-angular";
 export class CardapioPage implements OnInit {
     tipos = [
         {
-            nome: 'Comidas', itens: [
-                { nome: 'Petit gâteau', sub: 'cholate', img: 'assets/imgs/gato2.jpg'   , preco: 20.90 },
-                { nome: 'Nhoque', sub: 'Frito, 2 molhos', img: 'assets/imgs/nhoque.jpg', preco: 20.90 },
-                { nome: 'Pastelzinho', sub: 'Frito', img: 'assets/imgs/pastelzinho.jpg', preco: 20.90 },
-                { nome: 'Pizza', sub: '2 sabores', img: 'assets/imgs/pizza.jpg'        , preco: 20.90 }
+            nome: 'Panquecas', itens: [
+                {cod: 512,  nome: 'Brócolis',                               sub: 'brócolis, alho e bacon',                              img: 'assets/imgs/panqueca_brocolis.jpg'   , preco: 19.5 },
+                {cod: 51 ,  nome: 'Carne',                                  sub: 'carne moída com bacon',                               img: 'assets/imgs/panqueca_carne.jpg', preco: 16.5 },
+                {cod: 53 ,  nome: 'Carne com Catupiry',                     sub: 'carne moída com catupiry',                            img: 'assets/imgs/panqueca_carne_catupiry.jpg', preco: 19.5 },
+                {cod: 52 ,  nome: 'Carne com Queijo',                       sub: 'carne moída com queijo',                              img: 'assets/imgs/panqueca_carne_queijo.jpg'        , preco: 19.5 },
+                {cod: 56 ,  nome: 'Frango',                                 sub: 'filé de frango em cubos',                             img: 'assets/imgs/panqueca_frango.jpg'        , preco: 16.5 },
+                {cod: 57 ,  nome: 'Frango com Queijo',                      sub: 'filé de frango com queijo',                           img: 'assets/imgs/panqueca_frango_queijo.jpg'        , preco: 19.5 },
+                {cod:  58,  nome: 'Frango com Catupiry',                    sub: 'filé de frango com catupiry',                         img: 'assets/imgs/panqueca_frango_catupiry.jpg'        , preco: 19.5 },
+                {cod:  55,  nome: 'SUPER CHICK',                            sub: 'strogonoff de carne, cheddar e palha',            img: 'assets/imgs/panqueca_super_chick.jpg'        , preco: 22.5 },
+                {cod:  515, nome: 'Rucula com tomate seco',                 sub: 'rucula, mussarela e tomate seco',                 img: 'assets/imgs/panqueca_rucula.jpg'        , preco: 20.5 },
+                {cod:  513, nome: 'Francesa',                               sub: 'presunto, mussarela, catupiry e champignon',          img: 'assets/imgs/panqueca_presunto.jpg'        , preco: 20.5 },
+                {cod:  54,  nome: 'Strogonoff Carne',                       sub: 'stogonoff carne com batata palha',                    img: 'assets/imgs/panqueca_strogonoff.jpg'        , preco: 22.5 },
+                {cod:  59,  nome: 'Strogonoff Frango',                      sub: 'strogonoff frango com batata palha',                  img: 'assets/imgs/panqueca_strogonoff.jpg'        , preco: 22.5 },
+                {cod:  511, nome: 'Mexicana',                               sub: 'carne moída, pimenta biquinho e queijo',                  img: 'assets/imgs/panqueca_mexicana.jpg'        , preco: 20.5 },
+                {cod:  518, nome: 'Bahiana',                                sub: 'calabresa acebolada e queijo. Com toque apimentado',       img: 'assets/imgs/panqueca_bahiana.jpg'        , preco: 19.5 },
+                {cod:  517, nome: 'Caipira',                                sub: 'frango em cubos, milho verde e queijo',                   img: 'assets/imgs/panqueca_caipira.jpg'        , preco: 19.5 },
             ]
         },
-        {
-            nome: 'Bebidas', itens: [
-                { nome: 'Coca cola', sub: 'Lata', img: 'assets/imgs/coca.jpg', preco: '5.00' },
-                { nome: 'Cerveja', sub: '600 ml', img: 'assets/imgs/cerveja.jpg', preco: '6.90' }
-            ]
-        }
+        // {
+        //     nome: 'Bebidas', itens: [
+        //         { nome: 'Coca cola', sub: 'Lata', img: 'assets/imgs/coca.jpg', preco: '5.00' },
+        //         { nome: 'Cerveja', sub: '600 ml', img: 'assets/imgs/cerveja.jpg', preco: '6.90' }
+        //     ]
+        // }
     ]
 
     constructor(public navCtrl: NavController) {
 
     }
 
-    goDetalhes(){
-    this.navCtrl.push("DescricaoPage");
+    goDetalhes(cod){
+    this.navCtrl.push("DescricaoPage", {codigo: cod});
     }
     ngOnInit() {
 
--- src/pages/descricao/descricao.ts
+++ src/pages/descricao/descricao.ts
@@ -12,11 +12,14 @@ import { CheckoutModal } from '../checkout/checkout';
 @IonicPage()
 @Component({
   selector: 'page-descricao',
-  templateUrl: 'descricao.html',
+  templateUrl: 'descricao.html'
 })
 export class DescricaoPage {
 
+  codigo;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
+    this.codigo = this.navParams.get('codigo');
   }
 
   ionViewDidLoad() {
@@ -27,5 +30,86 @@ export class DescricaoPage {
     let profileModal = this.modalCtrl.create(CheckoutModal);
     profileModal.present();
   }
+
+  get image(): string {
+    switch (this.codigo) {
+        case 512:  return 'assets/imgs/panqueca_brocolis.jpg'       ;
+      	case 51 :  return 'assets/imgs/panqueca_carne.jpg'          ;
+      	case 53 :  return 'assets/imgs/panqueca_carne_catupiry.jpg' ;
+      	case 52 :  return 'assets/imgs/panqueca_carne_queijo.jpg'   ;
+      	case 56 :  return 'assets/imgs/panqueca_frango.jpg'         ;
+      	case 57 :  return 'assets/imgs/panqueca_frango_queijo.jpg'  ;
+      	case 58 :  return 'assets/imgs/panqueca_frango_catupiry.jpg';
+      	case 55 :  return 'assets/imgs/panqueca_super_chick.jpg'    ;
+      	case 515:  return 'assets/imgs/panqueca_rucula.jpg'         ;
+      	case 513:  return 'assets/imgs/panqueca_presunto.jpg'       ;
+      	case 54 :  return 'assets/imgs/panqueca_strogonoff.jpg'     ;
+      	case 59 :  return 'assets/imgs/panqueca_strogonoff.jpg'     ;
+      	case 511:  return 'assets/imgs/panqueca_mexicana.jpg'       ;
+      	case 518:  return 'assets/imgs/panqueca_bahiana.jpg'        ;
+      	case 517:  return 'assets/imgs/panqueca_caipira.jpg'        ;
+    }
+  }
+
+  get preco(): number {
+    switch (this.codigo) {
+        case 512:  return 19.5;
+        case 51 :  return 16.5;
+        case 53 :  return 19.5;
+        case 52 :  return 19.5;
+        case 56 :  return 16.5;
+        case 57 :  return 19.5;
+        case 58 :  return 19.5;
+        case 55 :  return 22.5;
+        case 515:  return 20.5;
+        case 513:  return 20.5;
+        case 54 :  return 22.5;
+        case 59 :  return 22.5;
+        case 511:  return 20.5;
+        case 518:  return 19.5;
+        case 517:  return 19.5;
+    }
+  }
+
+  get nome(): string {
+    switch (this.codigo) {
+        case 512:  return     'Brócolis'   ;
+        case 51 :  return 'Carne'      ;
+        case 53 :  return 'Carne com Catupiry'   ;
+        case 52 :  return 'Carne com Queijo'      ;
+        case 56 :  return 'Frango'            ;
+        case 57 :  return 'Frango com Queijo'    ;
+        case 58 :  return 'Frango com Catupiry'  ;
+        case 55 :  return 'SUPER CHICK'          ;
+        case 515:  return 'Rucula com tomate seco';
+        case 513:  return 'Francesa'         ;
+        case 54 :  return 'Strogonoff Carne'     ;
+        case 59 :  return 'Strogonoff Frango'    ;
+        case 511:  return 'Mexicana'          ;
+        case 518:  return 'Bahiana'           ;
+        case 517:  return 'Caipira'           ;
+    }
+  }
+
+  get sub(): string {
+    switch (this.codigo) {
+        case 512:  return 'brócolis; alho e bacon';                           
+        case 51 :  return 'carne moída com bacon';                            
+        case 53 :  return 'carne moída com catupiry';                         
+        case 52 :  return 'carne moída com queijo';                           
+        case 56 :  return 'filé de frango em cubos';                          
+        case 57 :  return 'filé de frango com queijo';                        
+        case 58 :  return 'filé de frango com catupiry';                      
+        case 55 :  return 'strogonoff de carne; cheddar e palha';            
+        case 515:  return 'rucula; mussarela e tomate seco';                 
+        case 513:  return 'presunto; mussarela; catupiry e champignon';       
+        case 54 :  return 'stogonoff carne com batata palha';                 
+        case 59 :  return 'strogonoff frango com batata palha';               
+        case 511:  return 'carne moída; pimenta biquinho e queijo';           
+        case 518:  return 'calabresa acebolada e queijo. Com toque apimentado'
+        case 517:  return 'frango em cubos; milho verde e queijo';            
+    }
+  }
+
 
 }
